37b4ec4142c533e79bd90d811284afe9a0d7dfa8
bm4656 <newjin46@gmail.com>
2023-05-09 23:41:14 +0900

    refactor: profile 컴포넌트

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -5,12 +5,23 @@ import ContactButton from './ContactButton';
 
 export default function Profile() {
   return (
-    <article className='flex flex-col justify-center items-center gap-1'>
-      <Image src={profileImage} alt='profile image' className='rounded-full' />
-      <h2 className='text-xl font-bold'>{"Hi, Im' Bomin"}</h2>
-      <p>Frontend Enginner</p>
-      <p>나에 대한 간단한 소개</p>
-      <ContactButton />
-    </article>
+    <section className='text-center'>
+      <Image
+        src={profileImage}
+        alt='profile image'
+        width={250}
+        height={300}
+        className='mx-auto rounded-full'
+      />
+      <h2 className='text-2xl font-bold mt-2'>{"Hi, Im' Bomin"}</h2>
+      <p className='text-xl font-semibold'>Frontend Enginner</p>
+      <p className='mb-2'>나에 대한 간단한 소개</p>
+      <Link
+        href='/contact'
+        className='bg-teal-400 text-white rounded-md p-1 text-sm font-semibold'
+      >
+        Contact me!
+      </Link>
+    </section>
   );
 }
