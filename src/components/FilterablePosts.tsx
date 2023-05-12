35583f4231234ab5643c405b193085d7a84dab19
bm4656 <newjin46@gmail.com>
2023-05-12 16:42:39 +0900

    design: posts 페이지 스타일링 1차

--- src/components/FilterablePosts.tsx
+++ src/components/FilterablePosts.tsx
@@ -6,23 +6,26 @@ import PostGrid from './PostGrid';
 type Props = { categories: string[]; posts: Post[] };
 export default function FilterablePosts({ categories, posts }: Props) {
   const [selected, setSelected] = useState('');
-  //1. 카테고리 누르면 selected 변수에 맞게 posts 필터링 되도록
-  //2. 필터링된 posts 들 PostGrid에 전달해서 화면에 나타내기
   const filtered =
     selected !== ''
       ? posts.filter((post) => post.category === selected)
       : posts;
   console.log(filtered);
   return (
-    <section>
-      <ul>
+    <section className='flex'>
+      <PostGrid posts={filtered}></PostGrid>
+      <ul className='flex flex-col mx-4 items-center'>
+        <h3 className='text-lg font-bold'>Category</h3>
         {categories.map((category, index) => (
-          <li key={index} onClick={() => setSelected(category)}>
+          <li
+            key={index}
+            onClick={() => setSelected(category)}
+            className='cursor-pointer'
+          >
             {category}
           </li>
         ))}
       </ul>
-      <PostGrid posts={filtered}></PostGrid>
     </section>
   );
 }
